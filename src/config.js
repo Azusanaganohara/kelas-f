42cd354ed8c7dec7e0603cc9e4770e5bd4fe880e
AyakashiFansub <ayakashifansub.id@gmail.com>
2024-06-25 06:01:15 +0700

    first commit

---
+++ src/config.js
@@ -0,0 +1,43 @@
+module.exports = {
+    client: {
+        token: 'MTI1NDY3NDQ1MDczODA1MzIwMA.GSlXSi.Wf8mKwWgwKhAzU8IIs_gynt19baX59bseveBHs',
+        id: '1254674450738053200',
+        guild: '991519513176723467',
+        database: 'mongodb://fxazusax:fxazusax@ac-o87sbev-shard-00-00.qmpvodp.mongodb.net:27017,ac-o87sbev-shard-00-01.qmpvodp.mongodb.net:27017,ac-o87sbev-shard-00-02.qmpvodp.mongodb.net:27017/?ssl=true&replicaSet=atlas-piystm-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Kelasf'
+    },
+    embed: {
+        color: 0x8224e3,
+    },
+
+
+
+    api: {
+        lavalink: {
+            host: 'lava-v3.ajieblogs.eu.org',
+            port: 443,
+            password: 'https://dsc.gg/ajidevserver',
+            secure: true
+        },
+        spotify: {
+            id: 'bf94b09d8e9f4505a5126ce17d1858b8',
+            secret: 'da8fd21d4a164bb29daf9d6f7ee42e74'
+        }
+    },
+    youtube: {
+        id: '',
+        secret: ''
+    },
+    pixiv:{
+        username: 'yumekomik@gmail.com',
+        pw: 'Azusa27062004',
+        phpsessid: '92036263_dSkCJ8JTjZ5v8qCAvkcDkoHsTmboZrae'
+
+    },
+    anilist: {
+        endpoint: 'https://graphql.anilist.co'
+    },
+    weather: {
+            weatherApiKey: '33a6f7274d55487fa7e223256243005'
+    },
+    
+};
